22cdc10f34823254e77253459163ca99885644bc
ioikka <belgampaul@gmail.com>
2021-02-13 21:08:27 +0300

    Fixed startup error

--- src/main/java/io/ikka/invest/KInvestApplication.kt
+++ src/main/java/io/ikka/invest/KInvestApplication.kt
@@ -1,11 +1,9 @@
 package io.ikka.invest
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@EnableJpaRepositories
-@SpringBootApplication
+//@EnableJpaRepositories
+//@SpringBootApplication
 open class KInvestApplication
 
 fun main(args: Array<String>) {
