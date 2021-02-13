c9f21c212b180d25483c987e90652bd5be7d1dc7
ioikka <belgampaul@gmail.com>
2021-02-13 21:31:08 +0300

    Log properties at startup

--- src/main/java/io/ikka/invest/config/properties/TinkoffOpenApiProperties.java
+++ src/main/java/io/ikka/invest/config/properties/TinkoffOpenApiProperties.java
@@ -2,13 +2,25 @@ package io.ikka.invest.config.properties;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
 @Setter
 @Getter
 @Configuration
 @ConfigurationProperties("io.ikka.ru.tinkoff")
 public class TinkoffOpenApiProperties {
     private String token;
+
+
+    @PostConstruct
+    public void logSettings() {
+        log.info("*********** TinkoffOpenApiProperties ***********");
+        log.info("token = {}***********", token.substring(0, 2));
+        log.info("************************************************");
+    }
 }
